4e6c566bf1388a0455eeda517984ac477f32f7b5
nci-dmccashin <x25427452@student.ncirl.ie>
2025-12-01 13:43:18 +0000

    Added basic calculator logic

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -6,4 +6,22 @@ document.getElementById("calculator-form").addEventListener("submit", (e) => {
 		// Get checked radio button value
 		let sizeOfBottles = document.querySelector("input[name=question2]:checked").value;
 		let doesUserReturnBottles = document.querySelector("input[name=question3]:checked").value;
+		let plasticUsedPerBottle = 0;
+		// Switch statement used to calculate how much plastic is wasted depending on size of the bottle
+		switch (sizeOfBottles) {
+			case "500":
+				plasticUsedPerBottle = 20;
+				break;
+			case "750":
+				plasticUsedPerBottle = 30;
+				break;
+			case "1000":
+				plasticUsedPerBottle = 35;
+				break;
+			case "2000":
+				plasticUsedPerBottle = 50;
+				break;
+		}
+		let plasticUsed = plasticUsedPerBottle * numberOfBottles;
+		alert(plasticUsed);
 });
