f6cdb2b600d6a76e80c012a3843b238c275f1270
nci-dmccashin <x25427452@student.ncirl.ie>
2025-12-04 14:18:25 +0000

    Added carbon impact to calculator and added different message if user says they return their bottles

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -34,22 +34,56 @@ document.getElementById("calculator-form").addEventListener("submit", (e) => {
 			case "500":
 				costOfDepositPerBottle = 15;
 				break;
+			// Cost of deposit is the same if over 500ml so no need to add switch case for each size
 			default:
 				costOfDepositPerBottle = 25;
 				break;
 		}
-		// Deposit only applies if user does not return bottles, so leave at 0 if they do
-		if (doesUserReturnBottles == "false") {
-			costOfDeposit = numberOfBottles * costOfDepositPerBottle;
+		let carbonImpactPerBottle = 0;
+		switch (sizeOfBottles) {
+			case "500":
+				carbonImpactPerBottle = 120;
+				break;
+			case "750":
+				carbonImpactPerBottle = 230;
+				break;
+			case "1000":
+				carbonImpactPerBottle = 280;
+				break;
+			case "2000":
+				carbonImpactPerBottle = 580;
+				break;
 		}
+		let carbonImpact = carbonImpactPerBottle * numberOfBottles;
+		costOfDeposit = numberOfBottles * costOfDepositPerBottle;
 		// Remove calculator and show user results
 		// Use template literal to format HTML
 		let costOfDepositInEuro = costOfDeposit / 100;
-		alert(costOfDeposit);
-		alert(costOfDepositInEuro);
-		document.getElementById("calculator-form").innerHTML = 
+		let calculatorFormElement = document.getElementById("calculator-form");
+		calculatorFormElement.innerHTML = 
 		`
 			<h1>You used ${plasticUsed}g of plastic in the last week</h1>
-			<h3>You wasted &euro;${costOfDepositInEuro} by not returning your bottles</h3>
+		`;
+		calculatorFormElement.innerHTML += 
+		`
+			<h3>The production of these bottles emiited ${carbonImpact}g of Co2 into our enviroment</h3>
+		`;
+		if (doesUserReturnBottles == "false") {
+			calculatorFormElement.innerHTML += 
+			`
+				<h3>You wasted &euro;${costOfDepositInEuro} by not returning your bottles</h3>
+			`;
+		} else {
+			calculatorFormElement.innerHTML += 
+			`
+				<h3>You saved &euro;${costOfDepositInEuro} by returning your bottles. Good Job!</h3>
+			`;
+		}
+		//TODO: Make link go to correct page
+		// Using JavaScript to open link instead of <a> is not ideal but it works
+		calculatorFormElement.innerHTML += 
+		`
+			<button class="btn btn-success" onclick="window.location.reload();">Restart</button><br/>
+			<button class="btn btn-primary mt-4" onclick="window.open('#');">How can I reduce my enviromental impact?</button>
 		`;
 });
