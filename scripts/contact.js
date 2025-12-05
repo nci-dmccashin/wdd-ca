35ff089b39f37f0890edcbdf595e28b7afd6b597
nci-dmccashin <x25427452@student.ncirl.ie>
2025-12-05 16:05:30 +0000

    Changed contact page logic

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,7 +3,7 @@ document.getElementById("contact-form").addEventListener("submit", (e) => {
 	let name = document.getElementById("form-input-name").value;
 	let email = document.getElementById("form-input-email").value;
 	let message = document.getElementById("form-input-message").value;
-	let url = `mailto:x25427452@student.ncirl.ie?subject=Message from ${name}&body=${message}&cc=${email}`;
+	let url = `mailto:x25427452@student.ncirl.ie?subject=Message from ${name}&body=${message}&reply-to=${email}`;
 	// Use encodeURI to account for spaces in URL
 	let encodedURL = encodeURI(url);
 	window.open(encodedURL);
